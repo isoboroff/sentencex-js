9beab60b70c9626280e9775391769b52685a74b2
soboroff <ian.soboroff@nist.gov>
2025-05-19 14:27:13 -0400

    assert -> with

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,30 @@
-import pkg from './package.json' assert { type: 'json' }
-import json from '@rollup/plugin-json'
-import esbuild from 'rollup-plugin-esbuild'
+import pkg from "./package.json" with { type: "json" };
+import json from "@rollup/plugin-json";
+import esbuild from "rollup-plugin-esbuild";
 
 export default [
   {
-    input: 'src/index.js',
+    input: "src/index.js",
     output: [
       {
         name: pkg.name,
         file: pkg.module,
-        format: 'esm',
-        sourcemap: true
+        format: "esm",
+        sourcemap: true,
       },
       {
         name: pkg.name,
         file: pkg.main,
-        format: 'umd',
-        sourcemap: true
-      }
+        format: "umd",
+        sourcemap: true,
+      },
     ],
     plugins: [
       json(),
       esbuild({
         sourceMap: true,
-        minify: true
-      })
-
-    ]
-  }
-]
+        minify: true,
+      }),
+    ],
+  },
+];
